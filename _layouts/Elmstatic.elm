8444f8b6594a2aee856431099d8ed3b304f72b88
agj <ale@agj.cl>
2021-06-19 20:15:41 -0400

    Attempt to prune elm-static default installation

--- _layouts/Elmstatic.elm
+++ _layouts/Elmstatic.elm
@@ -1,55 +1,20 @@
 module Elmstatic exposing
     ( Content
-    , Format(..)
     , Layout
     , Page
-    , Post
-    , PostList
-    , decodePage
-    , decodePost
-    , decodePostList
     , htmlTemplate
-    , inlineScript
     , layout
-    , script
-    , stylesheet
     )
 
 import Browser
+import Element exposing (Element)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Json.Decode as Json
 
 
-type Format
-    = Markdown
-    | ElmMarkup
-
-
-type alias Post =
-    { content : String
-    , date : String
-    , format : Format
-    , link : String
-    , section : String
-    , siteTitle : String
-    , tags : List String
-    , title : String
-    }
-
-
-type alias Page =
-    { content : String
-    , format : Format
-    , siteTitle : String
-    , title : String
-    }
-
-
-type alias PostList =
-    { posts : List Post
-    , section : String
-    , siteTitle : String
+type alias Page msg =
+    { content : Element msg
     , title : String
     }
 
@@ -62,76 +27,6 @@ type alias Layout =
     Program Json.Value Json.Value Never
 
 
-{-| For backward compatibility, look for the content either in `markdown` or `content` fields
--}
-decodeContent : Json.Decoder String
-decodeContent =
-    Json.oneOf [ Json.field "markdown" Json.string, Json.field "content" Json.string ]
-
-
-decodeFormat : Json.Decoder Format
-decodeFormat =
-    Json.oneOf
-        [ Json.map
-            (\format ->
-                if format == "emu" then
-                    ElmMarkup
-
-                else
-                    Markdown
-            )
-          <|
-            Json.field "format" Json.string
-        , Json.succeed Markdown
-        ]
-
-
-decodePage : Json.Decoder Page
-decodePage =
-    Json.map4 Page
-        decodeContent
-        decodeFormat
-        (Json.field "siteTitle" Json.string)
-        (Json.field "title" Json.string)
-
-
-decodePost : Json.Decoder Post
-decodePost =
-    Json.map8 Post
-        decodeContent
-        (Json.field "date" Json.string)
-        decodeFormat
-        (Json.field "link" Json.string)
-        (Json.field "section" Json.string)
-        (Json.field "siteTitle" Json.string)
-        (Json.field "tags" <| Json.list Json.string)
-        (Json.field "title" Json.string)
-
-
-decodePostList : Json.Decoder PostList
-decodePostList =
-    Json.map4 PostList
-        (Json.field "posts" <| Json.list decodePost)
-        (Json.field "section" Json.string)
-        (Json.field "siteTitle" Json.string)
-        (Json.field "title" Json.string)
-
-
-script : String -> Html Never
-script src =
-    node "citatsmle-script" [ attribute "src" src ] []
-
-
-inlineScript : String -> Html Never
-inlineScript js =
-    node "citatsmle-script" [] [ text js ]
-
-
-stylesheet : String -> Html Never
-stylesheet href =
-    node "link" [ attribute "href" href, attribute "rel" "stylesheet", attribute "type" "text/css" ] []
-
-
 htmlTemplate : String -> List (Html Never) -> Html Never
 htmlTemplate title contentNodes =
     node "html"
@@ -140,11 +35,6 @@ htmlTemplate title contentNodes =
             []
             [ node "title" [] [ text title ]
             , node "meta" [ attribute "charset" "utf-8" ] []
-            , script "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.1/highlight.min.js"
-            , script "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.1/languages/elm.min.js"
-            , inlineScript "hljs.initHighlightingOnLoad();"
-            , stylesheet "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.1/styles/default.min.css"
-            , stylesheet "//fonts.googleapis.com/css?family=Open+Sans|Proza+Libre|Inconsolata"
             ]
         , node "body" [] contentNodes
         ]
--- _layouts/Page.elm
+++ _layouts/Page.elm
@@ -1,81 +1,21 @@
-module Page exposing (footer, header, layout, main, markdown)
+module Page exposing (layout, main)
 
 import Elmstatic exposing (..)
 import Html exposing (..)
 import Html.Attributes as Attr exposing (alt, attribute, class, href, src)
-import Markdown
 import Styles
-
-
-header : List (Html Never)
-header =
-    [ div [ class "header-logo" ]
-        [ img [ alt "Author's blog", src "/img/logo.png", attribute "width" "100" ]
-            []
-        ]
-    , div [ class "navigation" ]
-        [ ul []
-            [ li []
-                [ a [ href "/posts" ]
-                    [ text "Posts" ]
-                ]
-            , li []
-                [ a [ href "/about" ]
-                    [ text "About" ]
-                ]
-            , li []
-                [ a [ href "/contact" ]
-                    [ text "Contact" ]
-                ]
-            ]
-        ]
-    ]
-
-
-footer : Html Never
-footer =
-    div [ class "footer" ]
-        [ img
-            [ alt "Author's blog"
-            , src "/img/logo.png"
-            , attribute "style" "float: left; padding-top: 7px"
-            , attribute "width" "75"
-            ]
-            []
-        , div [ class "link" ]
-            [ githubIcon
-            , a [ href "https://github.com" ]
-                [ text "Author's GitHub" ]
-            ]
-        , div [ class "link" ]
-            [ twitterIcon
-            , a [ href "https://twitter.com" ]
-                [ text "Author's Twitter" ]
-            ]
-        , div [ class "link" ]
-            [ a [ href "https://www.npmjs.com/package/elmstatic" ]
-                [ text "Created with Elmstatic" ]
-            ]
-        ]
 
 
 layout : String -> List (Html Never) -> List (Html Never)
 layout title contentItems =
-    header
-        ++ [ div [ class "sidebar" ]
-                []
-           , div [ class "sidebar2" ]
-                []
-           , div [ class "content" ]
-                ([ h1 [] [ text title ] ] ++ contentItems)
-           , footer
-           , Elmstatic.stylesheet "/styles.css"
-           , Styles.styles
-           ]
+    [ div [ class "content" ]
+        ([ h1 [] [ text title ] ] ++ contentItems)
+    , Styles.styles
+    ]
 
 
 main : Elmstatic.Layout
 main =
     Elmstatic.layout Elmstatic.decodePage <|
         \content ->
-            Ok <| layout content.title [ markdown content.content ]
+            Ok <| layout content.title content.content
