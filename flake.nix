690033fd8c8dd002235ca4afe012f30d25763d7c
agj <ale@agj.cl>
2023-08-01 23:53:39 -0400

    Use Nix for dependencies

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Searches";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {system = system;};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            elmPackages.elm
+            elmPackages.elm-format
+            elmPackages.elm-test
+            nodejs_20
+            nodePackages.pnpm
+            nodePackages.prettier
+          ];
+        };
+      }
+    );
+}
